9c21d690010052ffcb0033bb090ea699830b31b5
claudio <cdjohnnatha@gmail.com>
2018-10-12 17:37:47 -0300

    update classify.py to send a json error in case to not found a word in vocabulary

--- lib/utils/classify.py
+++ lib/utils/classify.py
@@ -7,12 +7,17 @@ import sys
 nlp = spacy.load('pt')
 
 def classifyWords(words):
-  doc = nlp(words)
-  def parseJson(element):
-    return json.dumps({'word': element.orth_, 'classification': element.pos_ }, ensure_ascii=False)
-  classified = list(map(parseJson, doc))
+  try:
+    doc = nlp(words)
+    def parseJson(element):
+      if (element.orth_  in nlp.vocab) == False:
+        raise Exception(element.orth_)
+      return json.dumps({'word': element.orth_, 'classification': element.pos_ }, ensure_ascii=False)
+    classified = list(map(parseJson, doc))
 
-  return classified
+    return classified
+  except Exception as error:
+    return json.dumps({'error': 'Not found in vocabulary', 'word': error.args[0] }, ensure_ascii=False)
 
 tokens = str(sys.argv[1])
 if len(sys.argv) == 3:
