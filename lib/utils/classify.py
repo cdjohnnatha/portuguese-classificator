e88a52eddd125a2dfed2b17ea7cbcb31443f30cc
claudio <cdjohnnatha@gmail.com>
2018-10-16 22:26:26 -0300

    delete word verification in dictionary

--- lib/utils/classify.py
+++ lib/utils/classify.py
@@ -7,19 +7,15 @@ import sys
 nlp = spacy.load('pt')
 
 def classifyWords(words):
-  try:
-    doc = nlp(words)
-    def parseJson(element):
-      if (element.orth_  in nlp.vocab) == False:
-        raise Exception(element.orth_)
-      return json.dumps({'word': element.orth_, 'classification': element.pos_ }, ensure_ascii=False)
-    classified = list(map(parseJson, doc))
+  doc = nlp(words)
+  def parseJson(element):
+    return json.dumps({'word': element.orth_, 'classification': element.pos_ }, ensure_ascii=False)
+  classified = list(map(parseJson, doc))
 
-    return classified
-  except Exception as error:
-    return json.dumps({'error': 'Not found in vocabulary', 'word': error.args[0] }, ensure_ascii=False)
+  return classified
 
 tokens = str(sys.argv[1])
+
 if len(sys.argv) == 3:
   showPrint = sys.argv[2]
 else:
